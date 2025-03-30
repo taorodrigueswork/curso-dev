020f61a290059a48af542f0c532d0943a0b59171
Thiago Rodrigues <135357085+taorodrigueswork@users.noreply.github.com>
2025-03-30 18:26:14 +0000

    Enhance API status endpoint to return PostgreSQL database information and update package scripts for migrations

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 AS test");
-  response.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString(); //ISO 8601 format
+
+  // Query the PostgreSQL version
+  const versionResult = await database.query("SHOW server_version;");
+  const postgresVersion = versionResult.rows[0].server_version;
+
+  // Query the maximum number of connections
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  // Query the number of active connections
+  const activeConnectionsResult = await database.query(
+    "SELECT COUNT(*) AS active_connections FROM pg_stat_activity;",
+  );
+  const activeConnections = activeConnectionsResult.rows[0].active_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion, // Include PostgreSQL version in the response
+        max_connections: maxConnections, // Include max connections in the response
+        active_connections: activeConnections, // Include active connections in the response. If I send a wrong query, one new database connection will be opened.
+      },
+    },
+  });
 }
 
 export default status;
--- tests/get.test.js
+++ tests/get.test.js
@@ -1,8 +1,32 @@
-test("GET to /api/v1/status should return 200", async () => {
-  /*const response = fetch("/api/v1/status").then((response) => {
-    expect(response.status).toBe(200);
-  });*/
-
+test("GET to /api/v1/status should return 200 and include database info", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  // Check if updated_at exists and is in ISO format
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBeDefined(); //check if variable exists
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+  // Check if postgres_version exists and is a string
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+
+  // Check if max_connections exists and is a number
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(
+    Number.isInteger(
+      Number(responseBody.dependencies.database.max_connections),
+    ),
+  ).toBe(true);
+
+  // Check if active_connections exists and is a number
+  expect(responseBody.dependencies.database.active_connections).toBeDefined();
+  expect(
+    Number.isInteger(
+      Number(responseBody.dependencies.database.active_connections),
+    ),
+  ).toBe(true);
 });
